c157239a877bf0da614677bde817003f736840e5
Wan Saleh <wansaleh@gmail.com>
2020-09-23 21:10:32 +0800

    Updates.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/core';
 
-import './index.scss';
 import App from './App';
 import chakra from './styles/chakra';
+
+import './index.scss';
 
 ReactDOM.render(
   <ChakraProvider resetCSS theme={chakra}>
